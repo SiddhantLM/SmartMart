bd25654e6c756f366cd1ac5ceb1b631a3f61102e
Siddhant Phulwani <siddhantphulwani9@gmail.com>
2026-01-18 18:52:24 +0530

    Dotenv removed

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -3,14 +3,15 @@ package com.examly.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SpringappApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("D:\\programs\\JavaProject\\springapp\\.env").load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		// Dotenv dotenv =
+		// Dotenv.configure().directory("D:\\programs\\JavaProject\\springapp\\.env").load();
+		// dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 
 		SpringApplication.run(SpringappApplication.class, args);
 	}
