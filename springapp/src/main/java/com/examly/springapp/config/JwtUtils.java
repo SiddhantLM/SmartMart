979fa950f348b8fdb267e9b12527e06c6840f10a
Siddhant Phulwani <siddhantphulwani9@gmail.com>
2026-01-18 18:58:30 +0530

    Dotenv usage removed

--- springapp/src/main/java/com/examly/springapp/config/JwtUtils.java
+++ springapp/src/main/java/com/examly/springapp/config/JwtUtils.java
@@ -1,5 +1,5 @@
 package com.examly.springapp.config;
- 
+
 import java.util.Date;
 import java.util.function.Function;
 
@@ -10,37 +10,38 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
- 
 
 @Service
 public class JwtUtils {
-    Dotenv dotenv = Dotenv.configure().load();
-    String key = dotenv.get("JWT_SECRET");
-    // private String key=JWT_SECRET;
+    private final String key;
 
-    public String generateToken (String username) {
+    public JwtUtils(@Value("${jwt.secret}") String key) {
+        this.key = key;
+    }
+
+    public String generateToken(String username) {
         return Jwts.builder()
-                    .setSubject(username)
-                    .setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*60))
-                    .signWith(SignatureAlgorithm.HS256, key)
-                    .compact();
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
     }
- 
-    public Claims extractAllClaims (String token) {
+
+    public Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
- 
-    public <T> T extractClaim (String token, Function<Claims,T> function) {
+
+    public <T> T extractClaim(String token, Function<Claims, T> function) {
         Claims claims = extractAllClaims(token);
         return function.apply(claims);
     }
- 
-    public boolean validateToken (String token, String username) {
+
+    public boolean validateToken(String token, String username) {
         String existing = extractClaim(token, Claims::getSubject);
         return existing.equals(username);
     }
