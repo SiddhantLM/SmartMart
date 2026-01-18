ffc9a28e3a1e735dc6e899199315a73bfef3629a
Siddhant Phulwani <siddhantphulwani9@gmail.com>
2026-01-18 23:02:49 +0530

    email changed

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,10 +2,12 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 // import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringappApplication {
 
 	public static void main(String[] args) {
--- springapp/src/main/java/com/examly/springapp/service/EmailService.java
+++ springapp/src/main/java/com/examly/springapp/service/EmailService.java
@@ -1,38 +1,67 @@
 package com.examly.springapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
 
+    @Value("${sendgrid.from.email}")
+    private String fromEmail;
+
+    @Async
     public void sendOtpEmail(String toEmail, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Your OTP Code");
-        message.setText("Your OTP is: " + otp + "\nIt is valid for 10 minutes.");
-        mailSender.send(message);
+        sendMail(
+                toEmail,
+                "Your OTP Code",
+                "Your OTP is: " + otp + "\nIt is valid for 10 minutes.");
     }
-    
+
     public void sendStockReminderEmail(String toEmail, String productName) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Stock Reminder");
-        message.setText("Hello,\n\nThe product '" + productName + "' is back in stock!\n\nRegards,\nSmart Mart");
-        mailSender.send(message);
+        sendMail(
+                toEmail,
+                "Stock Reminder",
+                "Hello,\n\nThe product '" + productName + "' is back in stock!");
     }
 
     public void sendContactInfo(String info) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("sathyasailikhith@gmail.com");
-        message.setSubject("Contact");
-        message.setText(info);
-        mailSender.send(message);
+        sendMail(
+                "sathyasailikhith@gmail.com",
+                "Contact",
+                info);
     }
 
+    private void sendMail(String to, String subject, String content) {
+        Email from = new Email(fromEmail);
+        Email toEmail = new Email(to);
+        Content mailContent = new Content("text/plain", content);
+        Mail mail = new Mail(from, subject, toEmail, mailContent);
+
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sg.api(request);
+        } catch (Exception e) {
+            throw new RuntimeException("Email sending failed", e);
+        }
+    }
 }
