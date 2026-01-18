7ffbfcfa775352235ddab2d6464f379600520b31
Siddhant Phulwani <siddhantphulwani9@gmail.com>
2026-01-18 22:41:52 +0530

    entities changed

--- springapp/src/main/java/com/examly/springapp/model/Order.java
+++ springapp/src/main/java/com/examly/springapp/model/Order.java
@@ -1,5 +1,5 @@
 package com.examly.springapp.model;
- 
+
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -24,23 +24,23 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
- 
- 
+
 @Entity
-@Table (name = "`Order`")
-// @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "orderId")
+@Table(name = "`Order`")
+// @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
+// property = "orderId")
 public class Order {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     private double totalAmount;
     private int quantity;
     private LocalDate createdAt;
     private LocalDate updatedAt;
-   
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
-   
+
     public enum OrderStatus {
         PENDING,
         ACCEPTED,
@@ -51,27 +51,25 @@ public class Order {
         OUTFORDELIVERY,
         CANCELLED
     }
- 
- 
+
     @OneToOne
     @JoinColumn(name = "addressId")
     private Address address;
-   
+
     @ManyToOne
-    @JoinColumn(name="userId")
+    @JoinColumn(name = "userId")
     private User user;
-   
-   
-    @OneToMany(cascade = CascadeType.ALL)
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<OrderItem> items = new ArrayList<>();
- 
- 
+
+    @ManyToMany(mappedBy = "orders")
+    private List<Product> products = new ArrayList<>();
+
     public Order() {
     }
- 
-    
- 
+
     public Order(Long orderId, double totalAmount, int quantity, LocalDate createdAt, LocalDate updatedAt,
             OrderStatus status, Address address, User user, List<OrderItem> items) {
         this.orderId = orderId;
@@ -85,68 +83,66 @@ public class Order {
         this.items = items;
     }
 
-
-
     public Long getOrderId() {
         return orderId;
     }
- 
+
     public void setOrderId(Long orderId) {
         this.orderId = orderId;
     }
- 
+
     public Address getAddress() {
         return address;
     }
- 
+
     public void setAddress(Address address) {
         this.address = address;
     }
- 
+
     public double getTotalAmount() {
         return totalAmount;
     }
- 
+
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
- 
+
     public int getQuantity() {
         return quantity;
     }
- 
+
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
- 
+
     public LocalDate getCreatedAt() {
         return createdAt;
     }
- 
+
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
     }
- 
+
     public LocalDate getUpdatedAt() {
         return updatedAt;
     }
- 
+
     public void setUpdatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
     }
- 
+
     public OrderStatus getStatus() {
         return status;
     }
- 
+
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
- 
+
     public User getUser() {
         return user;
     }
- 
+
     public void setUser(User user) {
         this.user = user;
     }
--- springapp/src/main/java/com/examly/springapp/model/Product.java
+++ springapp/src/main/java/com/examly/springapp/model/Product.java
@@ -1,10 +1,10 @@
 package com.examly.springapp.model;
- 
+
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -16,14 +16,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
- 
- 
+
 @Entity
 public class Product {
- 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
@@ -34,135 +34,130 @@ public class Product {
     private String category;
     @Column(columnDefinition = "LONGTEXT")
     private String photoImage;
- 
-    //RATING AND REVIEWS
+
+    // RATING AND REVIEWS
     private int rating;
-   
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.REMOVE, orphanRemoval = true)
     @JsonManagedReference
     private List<Review> reviews = new ArrayList<>();
- 
+
     private LocalDate createdAt;
     private LocalDate updatedAt;
-   
+
     @ManyToOne
-    @JoinColumn(name="userId")
+    @JoinColumn(name = "userId")
     @JsonBackReference
     private User user;
- 
+
     @ManyToMany
+    @JoinTable(name = "product_orders", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "order_id"))
     private List<Order> orders = new ArrayList<>();
-   
+
     public Long getProductId() {
         return productId;
     }
- 
+
     public int getRating() {
         return rating;
     }
- 
+
     public void setRating(int rating) {
         this.rating = rating;
     }
- 
+
     public List<Review> getReviews() {
         return reviews;
     }
- 
+
     public void setReviews(List<Review> reviews) {
         this.reviews = reviews;
     }
- 
+
     public void setProductId(Long productId) {
         this.productId = productId;
     }
- 
+
     public String getName() {
         return name;
     }
- 
+
     public void setName(String name) {
         this.name = name;
     }
- 
+
     public String getDescription() {
         return description;
     }
- 
+
     public void setDescription(String description) {
         this.description = description;
     }
- 
+
     public Double getPrice() {
         return price;
     }
- 
+
     public void setPrice(Double price) {
         this.price = price;
     }
- 
+
     public int getStock() {
         return stock;
     }
- 
+
     public void setStock(int stock) {
         this.stock = stock;
     }
- 
+
     public String getCategory() {
         return category;
     }
- 
+
     public void setCategory(String category) {
         this.category = category;
     }
- 
+
     public String getPhotoImage() {
         return photoImage;
     }
- 
+
     public void setPhotoImage(String photoImage) {
         this.photoImage = photoImage;
     }
- 
+
     public LocalDate getCreatedAt() {
         return createdAt;
     }
- 
+
     public LocalDate getUpdatedAt() {
         return updatedAt;
     }
- 
+
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
     }
- 
+
     public void setUpdatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
     }
- 
+
     public User getUser() {
         return user;
     }
- 
+
     public void setUser(User user) {
         this.user = user;
     }
- 
+
     public Product() {
     }
- 
-   
- 
- 
- 
+
     public List<Order> getOrders() {
         return orders;
     }
- 
+
     public void setOrders(List<Order> orders) {
         this.orders = orders;
     }
 }
- 
