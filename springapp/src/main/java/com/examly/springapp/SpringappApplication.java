b45d90f8c12b91d7ea3e762c3202e460f8ccc15d
Siddhant Phulwani <siddhantphulwani9@gmail.com>
2026-01-23 00:09:01 +0530

    build comm

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -10,8 +10,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class SpringappApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("D:\\programs\\JavaProject\\springapp\\.env").load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+		// Dotenv dotenv =
+		// Dotenv.configure().directory("D:\\programs\\JavaProject\\springapp\\.env").load();
+		// dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 
 		SpringApplication.run(SpringappApplication.class, args);
 	}
